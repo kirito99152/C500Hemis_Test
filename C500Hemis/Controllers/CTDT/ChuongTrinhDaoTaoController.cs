da30ad34dafb7c6fd75b44b105a0cd1f7117b843
Dang Phuc <ndpxiv05@gmail.com>
2024-11-08 09:09:52 +0700

    zbỏ

--- C500Hemis/Controllers/CTDT/ChuongTrinhDaoTaoController.cs
+++ C500Hemis/Controllers/CTDT/ChuongTrinhDaoTaoController.cs
@@ -13,16 +13,19 @@ namespace C500Hemis.Controllers.CTDT
     {
         private readonly HemisContext _context;
 
+        // Lấy từ HemisContext 
         public ChuongTrinhDaoTaoController(HemisContext context)
         {
             _context = context;
         }
 
         // GET: ChuongTrinhDaoTao
-        public async Task<IActionResult> Index() // lấy 
+        // Lấy danh sách CTĐT từ database, trả về view Index.
+        public async Task<IActionResult> Index() 
         {
             try {
-                var hemisContext = _context.TbChuongTrinhDaoTaos // truy xuất dữ liệu từ database
+                var hemisContext = _context.TbChuongTrinhDaoTaos 
+                // Lấy data từ các table khác có liên quan (khóa ngoài) để hiển thị trên Index
                 .Include(t => t.IdDonViCapBangNavigation)
                 .Include(t => t.IdHocCheDaoTaoNavigation)
                 .Include(t => t.IdLoaiChuongTrinhDaoTaoNavigation)
@@ -31,7 +34,9 @@ namespace C500Hemis.Controllers.CTDT
                 .Include(t => t.IdQuocGiaCuaTruSoChinhNavigation)
                 .Include(t => t.IdTrangThaiCuaChuongTrinhNavigation)
                 .Include(t => t.IdTrinhDoDaoTaoNavigation);
-                return View(await hemisContext.ToListAsync()); // trả về View danh sách các CTĐT
+                return View(await hemisContext.ToListAsync()); 
+                
+            // Bắt lỗi các trường hợp ngoại lệ
             } catch ( Exception ex ) 
             {
                 return BadRequest(); 
@@ -39,7 +44,8 @@ namespace C500Hemis.Controllers.CTDT
             
         }
 
-        // GET: ChuongTrinhDaoTao/Details/5
+        // Lấy chi tiết 1 bản ghi dựa theo ID tương ứng đã truyền vào (IdChuongTrinhDaoTao)
+        // Hiển thị bản ghi đó ở view Details
         public async Task<IActionResult> Details(int? id)
         {
             try {
@@ -48,6 +54,7 @@ namespace C500Hemis.Controllers.CTDT
                     return NotFound();
                 }
 
+                // Tìm các dữ liệu theo Id tương ứng đã truyền vào view Details
                 var tbChuongTrinhDaoTao = await _context.TbChuongTrinhDaoTaos
                     .Include(t => t.IdDonViCapBangNavigation)
                     .Include(t => t.IdHocCheDaoTaoNavigation)
@@ -58,10 +65,14 @@ namespace C500Hemis.Controllers.CTDT
                     .Include(t => t.IdTrangThaiCuaChuongTrinhNavigation)
                     .Include(t => t.IdTrinhDoDaoTaoNavigation)
                     .FirstOrDefaultAsync(m => m.IdChuongTrinhDaoTao == id);
+                
+                // Nếu không tìm thấy Id tương ứng, chương trình sẽ báo lỗi NotFound
                 if (tbChuongTrinhDaoTao == null)
                 {
                     return NotFound();
                 }
+                // Nếu đã tìm thấy Id tương ứng, chương trình sẽ dẫn đến view Details
+                // Hiển thị thông thi chi tiết CTĐT thành công
                 return View(tbChuongTrinhDaoTao);
             } catch (Exception ex) 
             { 
@@ -69,8 +80,10 @@ namespace C500Hemis.Controllers.CTDT
             }
             
         }
-
+        
         // GET: ChuongTrinhDaoTao/Create
+        // Hiển thị view Create để tạo một bản ghi CTĐT mới
+        // Truyền data từ các table khác hiển thị tại view Create (khóa ngoài)
         public IActionResult Create()
         {
             try {
@@ -94,8 +107,12 @@ namespace C500Hemis.Controllers.CTDT
         // POST: ChuongTrinhDaoTao/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
+        // Thêm một CTĐT mới vào Database nếu IdChuongTrinhDaoTao truyền vào không trùng với Id đã có trong Database
+        // Trong trường hợp nhập trùng IdChuongTrinhDaoTao sẽ bắt lỗi
+        // Bắt lỗi ngoại lệ sao cho người nhập BẮT BUỘC phải nhập khác IdChuongTrinhDaoTao đã có
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken] // Một phương thức bảo mật thông qua Token được tạo tự động cho các Form khác nhau
         public async Task<IActionResult> Create([Bind("IdChuongTrinhDaoTao,MaChuongTrinhDaoTao,IdNganhDaoTao,TenChuongTrinh,TenChuongTrinhBangTiengAnh,NamBatDauTuyenSinh,TenCoSoDaoTaoNuocNgoai,IdLoaiChuongTrinhDaoTao,IdLoaiChuongTrinhLienKetDaoTao,DiaDiemDaoTao,IdHocCheDaoTao,IdQuocGiaCuaTruSoChinh,NgayBanHanhChuanDauRa,IdTrinhDoDaoTao,ThoiGianDaoTaoChuan,ChuanDauRa,IdDonViCapBang,LoaiChungChiDuocChapThuan,DonViThucHienChuongTrinh,IdTrangThaiCuaChuongTrinh,ChuanDauRaVeNgoaiNgu,ChuanDauRaVeTinHoc,HocPhiTaiVietNam,HocPhiTaiNuocNgoai")] TbChuongTrinhDaoTao tbChuongTrinhDaoTao)
         {
             try {
@@ -127,7 +144,10 @@ namespace C500Hemis.Controllers.CTDT
             
         }
 
-        // GET: ChuongTrinhDaoTao/Edit/5
+        // GET: ChuongTrinhDaoTao/Edit
+        // Lấy data từ Database với Id đã có, sau đó hiển thị ở view Edit
+        // Nếu không tìm thấy Id tương ứng sẽ báo lỗi NotFound
+        // Phương thức này gần giống Create, nhưng nó nhập dữ liệu vào Id đã có trong database
         public async Task<IActionResult> Edit(int? id)
         {
             try {
@@ -157,11 +177,16 @@ namespace C500Hemis.Controllers.CTDT
             
         }
 
-        // POST: ChuongTrinhDaoTao/Edit/5
+        // POST: ChuongTrinhDaoTao/Edit
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        
+        // Lưu data mới (ghi đè) vào các trường Data đã có thuộc IdChuongTrinhDaoTao cần chỉnh sửa
+        // Nó chỉ cập nhật khi ModelState hợp lệ
+        // Nếu không hợp lệ sẽ báo lỗi, vì vậy cần có bắt lỗi.
+      
         [HttpPost]
-        [ValidateAntiForgeryToken]
+        [ValidateAntiForgeryToken] // Một phương thức bảo mật thông qua Token được tạo tự động cho các Form khác nhau
         public async Task<IActionResult> Edit(int id, [Bind("IdChuongTrinhDaoTao,MaChuongTrinhDaoTao,IdNganhDaoTao,TenChuongTrinh,TenChuongTrinhBangTiengAnh,NamBatDauTuyenSinh,TenCoSoDaoTaoNuocNgoai,IdLoaiChuongTrinhDaoTao,IdLoaiChuongTrinhLienKetDaoTao,DiaDiemDaoTao,IdHocCheDaoTao,IdQuocGiaCuaTruSoChinh,NgayBanHanhChuanDauRa,IdTrinhDoDaoTao,ThoiGianDaoTaoChuan,ChuanDauRa,IdDonViCapBang,LoaiChungChiDuocChapThuan,DonViThucHienChuongTrinh,IdTrangThaiCuaChuongTrinh,ChuanDauRaVeNgoaiNgu,ChuanDauRaVeTinHoc,HocPhiTaiVietNam,HocPhiTaiNuocNgoai")] TbChuongTrinhDaoTao tbChuongTrinhDaoTao)
         {
             try {
@@ -206,7 +231,10 @@ namespace C500Hemis.Controllers.CTDT
             
         }
 
-        // GET: ChuongTrinhDaoTao/Delete/5
+        // GET: ChuongTrinhDaoTao/Delete
+        // Xóa một CTĐT khỏi Database
+        // Lấy data CTĐT từ Database, hiển thị Data tại view Delete
+        // Hàm này để hiển thị thông tin cho người dùng trước khi xóa
         public async Task<IActionResult> Delete(int? id)
         {
             try
@@ -239,10 +267,11 @@ namespace C500Hemis.Controllers.CTDT
            
         }
 
-        // POST: ChuongTrinhDaoTao/Delete/5
+        // POST: ChuongTrinhDaoTao/Delete
+        // Xóa CTĐT khỏi Database sau khi nhấn xác nhận 
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> DeleteConfirmed(int id) // Lệnh xác nhận xóa hẳn một CTĐT
         {
             try {
                 var tbChuongTrinhDaoTao = await _context.TbChuongTrinhDaoTaos.FindAsync(id);
